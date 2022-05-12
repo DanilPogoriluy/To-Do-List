1f408d71afa32cf426cbb660b409178fa7a8dc6c
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-12 09:38:46 +0300

    Add project files.

---
+++ To-Do-List/Pages/Forms/CreateMission.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace To_Do_List.Pages.Forms
+{
+    public class CreateMissionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
