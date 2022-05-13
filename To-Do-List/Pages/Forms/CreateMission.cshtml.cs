11b377913ad61977c13bd0a2ff0ca635a138c25c
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-13 20:45:29 +0300

    Start creating PsevdoDB

--- To-Do-List/Pages/Forms/CreateMission.cshtml.cs
+++ To-Do-List/Pages/Forms/CreateMission.cshtml.cs
@@ -1,10 +1,13 @@
     using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using To_Do_List.Models;
 
 namespace To_Do_List.Pages.Forms
 {
     public class CreateMissionModel : PageModel
     {
+        List<Missions> MissionArray = new List<Missions>();
+
         public void OnGet()
         {
         }
