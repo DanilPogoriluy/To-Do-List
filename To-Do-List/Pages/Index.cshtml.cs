6df454521e10832467a79e56c29e544f87f18a3e
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-14 10:04:14 +0300

    Create simple Home Page and add PsevoDB functional

--- To-Do-List/Pages/Index.cshtml.cs
+++ To-Do-List/Pages/Index.cshtml.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using To_Do_List.Models;
 
 namespace To_Do_List.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public List<Missions> missions = new List<Missions>()
+        {
+            new Missions{Id = 1, DateTime = DateTime.Now, MissionText="Hello World" },
+            new Missions{Id = 2, DateTime = DateTime.Now, MissionText="I love u" }
+        };
 
         public IndexModel(ILogger<IndexModel> logger)
         {
