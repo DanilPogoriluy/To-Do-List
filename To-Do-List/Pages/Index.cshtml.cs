0c6edf8434962c0a3af1069ff0f443606f7fccb2
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-05-17 07:51:56 +0300

    Delete Privacy Page and Correct Index Page

--- To-Do-List/Pages/Index.cshtml.cs
+++ To-Do-List/Pages/Index.cshtml.cs
@@ -20,7 +20,6 @@ namespace To_Do_List.Pages
 
         public IActionResult OnPost()
         {
-            //Make Redirect Result in Delete Page
             return RedirectToPage("/Forms/Delete");
         }
 
